2f1eefa3f96e027d1119e0693ae99b039140efe0
arthur5775 <77300083+arthur5775@users.noreply.github.com>
2024-06-21 16:29:25 +0200

    Update F3F4use.py

--- F3F4use.py
+++ F3F4use.py
@@ -2,30 +2,29 @@ import os
 import sys
 import json
 import pandas as pd
-from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 import joblib
 
-def train_kmeans(df, n_clusters):
+def train_rdft(df, n_clusters):
     X = df[['haut_tot', 'tronc_diam']].values
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
-    kmeans = KMeans(n_clusters=n_clusters, random_state=0)
-    kmeans.fit(X_scaled)
-    return kmeans, scaler
+    rdft = KMeans(n_clusters=n_clusters, random_state=0)
+    rdft.fit(X_scaled)
+    return rdft, scaler
 
 def save_model(model, scaler, model_path, scaler_path):
     joblib.dump(model, model_path)
     joblib.dump(scaler, scaler_path)
 
 def load_model(model_path, scaler_path):
-    kmeans = joblib.load(model_path)
+    rdft = joblib.load(model_path)
     scaler = joblib.load(scaler_path)
-    return kmeans, scaler
+    return rdft, scaler
 
-def predict_cluster(kmeans, scaler, longitude, latitude, clc_secteur, haut_tot, tronc_diam, age_estim):
+def predict_cluster(rdft, scaler, longitude, latitude, clc_secteur, haut_tot, tronc_diam, age_estim):
     X_new = scaler.transform([[longitude, latitude, clc_secteur, haut_tot, tronc_diam, age_estim]])
-    cluster = kmeans.predict(X_new)
+    cluster = rdft.predict(X_new)
     return cluster[0]
 
 def main():
@@ -48,19 +47,19 @@ def main():
 
     try:
         # Essaie de charger le modèle et le scaler depuis le disque 
-        kmeans, scaler = load_model(model_path, scaler_path)
-        print("Model and scaler loaded from disk.")
+        rdft, scaler = load_model(model_path, scaler_path)
+        print("Modele et Scalar chargés depuis le disque.")
     except (FileNotFoundError, EOFError):
         print("pas de .pkl")
 
     # Prediction du cluster pour le nouveau point de données
-    cluster = predict_cluster(kmeans, scaler, longitude, latitude, clc_secteur, haut_tot, tronc_diam, age_estim)
+    cluster = predict_cluster(rdft, scaler, longitude, latitude, clc_secteur, haut_tot, tronc_diam, age_estim)
 
     # Ecrit le cluster prédit dans un fichier JSON
     with open('predicted_cluster.json', 'w') as json_file:
         json.dump(int(cluster), json_file)
 
-    print(f"Cluster number {int(cluster)} saved to predicted_cluster.json")
+    print(f"Numéro du cluster sauvegardé dans predicted_cluster.json: {int(cluster)}")
 
 if __name__ == "__main__":
     main()
